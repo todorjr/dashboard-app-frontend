72e67ffa127448b403ced1b1815d9dcd7a0785a3
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-09-22 16:21:16 +0200

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.png';
+
 
 function Header() {
     return (
         <div className="header">
+            <img src={logo} alt="Logo" className="logo" />
             <ul>
                 <li>Tab 1</li>
                 <li>Tab 2</li>
