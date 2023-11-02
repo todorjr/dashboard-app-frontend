a682b01f14f2e78e9c11e495d9bdaf460e8f8222
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-11-02 12:46:59 +0100

    fix: user id in react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import './styles.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard'
+import { getUserData } from './services/services';
 
 function App() {
+  const [userId, setUserId] = useState(18);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const userData = await getUserData(userId);
+      setUserId(userData.id);
+    }
+    fetchUserData();
+  }, []); 
+  
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Dashboard userId = { userId }/>,
+    },
+  ]);
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-    <Header />
-    <div style={{ display: 'flex' }}>
-      <Footer />
-      <Dashboard userId={12} />
-    </div>
-  </div>
+    <React.StrictMode>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <Header />
+        <div style={{ display: 'flex' }}>
+          <Footer />
+          <RouterProvider router={ router } />
+        </div>
+      </div>
+    </React.StrictMode>
+
   )
 }
 
 export default App
+
+
